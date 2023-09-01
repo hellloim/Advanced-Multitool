68cfe5d321257d1c8ebf78f3d2c091a74a5bb586
FarmorOnlyfans <79936930+hellloim@users.noreply.github.com>
2023-09-02 00:34:50 +0200

    Create install.bat
    Installs all the req for the multitool to work

---
+++ install.bat
@@ -0,0 +1,14 @@
+pip install colorama
+pip install faker
+pip install tqdm
+pip install tkinter
+pip install requests
+pip install sniffio=1.3.0
+pip install attrs==22.2.0
+pip install flask==2.2.2
+pip install httpx==0.23.3
+pip install py-cord
+pip install typing_extensions==4.4.0
+pip install scapy
+pip install beautifulsoup4
+pip install discord-webhook

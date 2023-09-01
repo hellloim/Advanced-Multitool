5ea947c496ded44dd3b5d84c36729d299632a1ee
FarmorOnlyfans <79936930+hellloim@users.noreply.github.com>
2023-09-02 00:31:12 +0200

    Create nitroGEN.py

---
+++ nitroGEN.py
@@ -0,0 +1,31 @@
+import random
+import string
+import requests
+import colorama
+
+def gencode():
+	letters = string.ascii_letters + string.digits
+	return ''.join(random.choice(letters) for i in range(19))
+
+
+class NitroGenerator:
+	def __init__(self):
+		self.codes = []
+		self.check()
+	
+	def check(self):
+		while True:
+			code = gencode()
+			self.codes.append(code)
+			response = requests.get(
+				"https://discord.com/api/v7/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
+			data = response.json()
+			if data["message"] == 'Unknown Gift Code':
+				print("Not Working: " +"https://discord.gift/"+ code)
+			else:
+				print("Possibly Working: " +"https://discord.gift/"+ code)
+				file = open("workedcodes.txt", "a+")
+				file.write("\n"+ code)
+
+
+NitroGenerator()
